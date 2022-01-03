fa0ace69479a6c95411e89918b540f36b5678385
Siddhantthorat08 <siddhant08thorat10@gmail.com>
2022-01-03 13:04:34 +0530

    Added photo upload functionality for Users through CarrierWave gem

--- db/migrate/20220103051432_create_images.rb
+++ db/migrate/20220103051432_create_images.rb
@@ -3,7 +3,7 @@ class CreateImages < ActiveRecord::Migration[6.1]
     create_table :images do |t|
       t.string :name
       t.string :picture
-      t.refrences :user
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
